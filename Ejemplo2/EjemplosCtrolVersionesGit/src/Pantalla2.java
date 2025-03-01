8528a9e274a08bc0dfc165a7e689b2dcbba8d733
GeroniMolero <gmolrod892@g.educaand.es>
2025-03-01 23:56:25 +0100

    Create Pantalla2.java

---
+++ Ejemplo2/EjemplosCtrolVersionesGit/src/Pantalla2.java
@@ -0,0 +1,10 @@
+package mensaje;
+
+public class Pantalla2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola buenas noches, esto es un 2º mensaje por pantalla");
+    System.out.println("Buenas de nuevo, esto es un 3er mensaje por pantalla");
+	}
+
+}
